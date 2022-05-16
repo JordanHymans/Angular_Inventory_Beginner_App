013e0bc2d3fa77adf83ee63ab2d3c0f621dd5cdf
Jordan McKenna <JMcKenna@hymans.co.uk>
2022-05-16 10:54:31 +0100

    full project

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscriber, Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
 @Component({
-    selector: 'pm-products',
     templateUrl:'./product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   // component class properties
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
 
     private _listFilter: string = '';
     get listFilter(): string{
@@ -31,6 +33,7 @@ export class ProductListComponent implements OnInit {
     constructor(private productService: ProductService) {
 
     }
+
     performFilter(filterBy: string): IProduct[] {
       filterBy = filterBy.toLocaleLowerCase();
       return this.products.filter((product:IProduct) =>
@@ -42,8 +45,17 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err,
+    });
+    }
+
+    ngOnDestroy(): void {
+      this.sub.unsubscribe();
     }
 
     onRatingClicked(message: string): void {
